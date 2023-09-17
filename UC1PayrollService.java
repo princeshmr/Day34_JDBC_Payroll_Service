64026065822bab581dcd5c50aeff2ab0e3a6f6d8
rohit789@ <princeshmr27@gmail.com>
2023-09-17 12:43:46 +0530

    Employee Peyroll Service using JDBC

---
+++ UC1PayrollService.java
@@ -0,0 +1,29 @@
+package com.day34JDBC;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UC1PayrollService {
+
+	public static void main(String[] args) {
+		String url="jdbc:mysql://localhost:3306/payroll_service";
+		String username="root";
+		String password="Sharma@15268";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		try {
+			DriverManager.getConnection(url, username, password);
+			System.out.println("Connection is established");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
